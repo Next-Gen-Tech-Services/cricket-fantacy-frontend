015183d8b2103b81029f5b850becb6b61fbe393b
ankit2103 <achawda866@gmail.com>
2026-01-29 12:30:16 +0530

    Update SplashScreen.jsx

--- src/components/SplashScreen.jsx
+++ src/components/SplashScreen.jsx
@@ -66,12 +66,7 @@ const SplashScreen = ({ onComplete, duration = 5000 }) => {
                     />
                 </div>
 
-                {/* Loading animation */}
-                <div className="flex justify-center items-center space-x-2">
-                    <div className="w-3 h-3 bg-orange-500 rounded-full animate-bounce"></div>
-                    <div className="w-3 h-3 bg-orange-500 rounded-full animate-bounce" style={{ animationDelay: '0.1s' }}></div>
-                    <div className="w-3 h-3 bg-orange-500 rounded-full animate-bounce" style={{ animationDelay: '0.2s' }}></div>
-                </div>
+                
 
                
             </div>
